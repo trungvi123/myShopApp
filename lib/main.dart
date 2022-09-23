c7bcbbad76eede7690ff5ee68a2772c7322f67ba
trungvi123 <trungvi123@gmail.com>
2022-09-23 17:01:23 +0700

    add toprightbadge

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:myshop/ui/products/product_detail_screen.dart';
-import 'package:myshop/ui/products/products_manager.dart';
-import 'ui/products/products_overview_screen.dart';
-import 'ui/products/user_products_screen.dart';
-import 'ui/cart/cart_screen.dart';
+// import 'package:myshop/ui/products/product_detail_screen.dart';
+// import 'package:myshop/ui/products/products_manager.dart';
+// import 'ui/products/products_overview_screen.dart';
+// import 'ui/products/user_products_screen.dart';
+// import 'ui/cart/cart_screen.dart';
+import 'ui/screens.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -24,9 +26,25 @@ class MyApp extends StatelessWidget {
           secondary: Colors.deepOrange,
         ),
       ),
-      home: const SafeArea(
-        child: CartScreen(),
-      ),
+      home: const ProductsOverviewScreen(),
+      routes: {
+        CartScreen.routeName: (ctx) => const CartScreen(),
+        OrdersScreen.routeName: (ctx) => const OrdersScreen(),
+        UserProductsScreen.routeName: (ctx) => const UserProductsScreen(),
+      },
+      onGenerateRoute: (settings) {
+        if (settings.name == ProductDetailScreen.routeName) {
+          final productId = settings.arguments as String;
+          return MaterialPageRoute(
+            builder: (ctx) {
+              return ProductDetailScreen(
+                ProductsManager().findById(productId),
+              );
+            },
+          );
+        }
+        return null;
+      },
     );
   }
 }
